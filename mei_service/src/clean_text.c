91289382cec40384e0306acc3d16ab87ad4fe2ad
Mark Meadows <mark@kyin.net>
2019-05-16 16:20:12 -0400

    end of day commit

--- mei_service/src/clean_text.c
+++ mei_service/src/clean_text.c
@@ -9,6 +9,21 @@ char * clean_text(char *text, int len_text)
 
 {
 	char clean_text[30] = {0};
+	int i = 0;
+	int a = 0;
+
+	while (i<len_text)
+	{
+		if (text[i] >= 48 && text[i]<=121)
+		{
+			clean_text[a]=text[i];
+			a++;
+		}
+
+		i++;
+	}
+
+	printf("Clean Text =%s \n",clean_text);
 
 	return clean_text;
 }
--- mei_service/src/detailed_info_command.c
+++ mei_service/src/detailed_info_command.c
@@ -5,10 +5,11 @@
  *      Author: mark
  */
 extern char tx_packet[30];
+extern char rx_packet[30];
+extern char comm_port[250];
 
 void detailed_info_command(void)
 {
-
 	tx_packet[0] = '\x02';
 	tx_packet[1] = '\x08';
 	tx_packet[2] = '\x60';
@@ -18,8 +19,15 @@ void detailed_info_command(void)
 	tx_packet[6] = '\x03';
 	tx_packet[7] = '\x6c';
 
+	mei_tx(tx_packet,comm_port);
+	mei_rx(comm_port);
+    clean_text(rx_packet,sizeof(rx_packet));
+    _
+
+
 
 
 
 	return;
+
 }
